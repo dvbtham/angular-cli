8548d9cbe3e4f40de017830809ec103ff891d239
Thâm Davies <thamdv96@gmail.com>
2017-08-29 22:21:13 +0700

    ngFor example.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   viewMode = 'otherwise';
+   courses = [
+     { id: 1 ,name: 'course 1'},
+     { id: 2 ,name: 'course 2'},
+     { id: 3 ,name: 'course 3'}
+   ];
+   newCourses;
+   onAdd(){
+     this.courses.push({ id: 4, name: 'course 4' });
+   }
+   onRemove(course){
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+    console.log(this.courses.length);
+  }
+  onChange(course){
+    let index = this.courses.indexOf(course);
+    this.courses[index].name = "updated";
+  }
+  loadCourses(){
+    this.newCourses = [
+      { id: 1 ,name: 'course 1'},
+      { id: 2 ,name: 'course 2'},
+      { id: 3 ,name: 'course 3'}
+    ];
+  }
+  trackCourse(index, course){
+    return course ? course.id : undefined;
+  }
 }
