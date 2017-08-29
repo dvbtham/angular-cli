1a7e923b9a9b08e7ce5fd4167562b3b1b4224820
Thâm Davies <thamdv96@gmail.com>
2017-08-29 22:43:45 +0700

    Safe Traversal Operator example.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,32 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   courses = [
-     { id: 1 ,name: 'course 1'},
-     { id: 2 ,name: 'course 2'},
-     { id: 3 ,name: 'course 3'}
-   ];
-   newCourses;
-   onAdd(){
-     this.courses.push({ id: 4, name: 'course 4' });
-   }
-   onRemove(course){
-    let index = this.courses.indexOf(course);
-    this.courses.splice(index, 1);
-    console.log(this.courses.length);
-  }
-  onChange(course){
-    let index = this.courses.indexOf(course);
-    this.courses[index].name = "updated";
-  }
-  loadCourses(){
-    this.newCourses = [
-      { id: 1 ,name: 'course 1'},
-      { id: 2 ,name: 'course 2'},
-      { id: 3 ,name: 'course 3'}
-    ];
-  }
-  trackCourse(index, course){
-    return course ? course.id : undefined;
+  task = {
+    title: 'Review Applications',
+    assignee: null
   }
 }
